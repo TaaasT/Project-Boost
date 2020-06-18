ffb80a1ffcaf7b4abb094e286762fa125ce3efeb
TaaasT <61253797+TaaasT@users.noreply.github.com>
2020-06-18 10:32:33 +0200

    Input basics

---
+++ Project Boost/Assets/Scripts/Rocket.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+
+        }
+        
+        if(Input.GetKey(KeyCode.A))
+        {
+
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+
+        }
+    }
+}
